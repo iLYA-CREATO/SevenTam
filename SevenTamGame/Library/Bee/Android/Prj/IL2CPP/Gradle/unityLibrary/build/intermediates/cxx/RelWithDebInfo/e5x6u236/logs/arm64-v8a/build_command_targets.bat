27f1a18e6e29937f3ad3b2c7f9e66a8c1062ed34
iLYA-CREATO <ilya.demidov.2003a@mail.ru>
2025-05-16 02:51:51 +0300

    add

---
+++ SevenTamGame/Library/Bee/Android/Prj/IL2CPP/Gradle/unityLibrary/build/intermediates/cxx/RelWithDebInfo/e5x6u236/logs/arm64-v8a/build_command_targets.bat
@@ -0,0 +1,6 @@
+@echo off
+"C:\\Program Files\\Unity\\Hub\\Editor\\6000.0.33f1\\Editor\\Data\\PlaybackEngines\\AndroidPlayer\\SDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "E:\\fails\\iT\\Isxodnik\\Git\\SevenTam\\SevenTamGame\\.utmp\\RelWithDebInfo\\e5x6u236\\arm64-v8a" ^
+  game ^
+  swappywrapper
